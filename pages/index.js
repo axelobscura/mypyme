4d37ea7522e2bfd1067ebfeb9c85693356590872
Axel <axosar@gmail.com>
2021-09-17 19:45:49 -0500

    dashboard inicio

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Layout from './components/Layout';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <Layout>
       <main className={styles.main}>
-        <img src="/logo.svg" alt="Cámara de comercio servicios y turismo de querétaro" title="Cámara de comercio servicios y turismo de querétaro" style={{
-          'width':'200px'
-        }} />
+        <Link href="/dashboard">
+          <img src="/logo.svg" alt="Cámara de comercio servicios y turismo de querétaro" title="Cámara de comercio servicios y turismo de querétaro" style={{
+            'width':'200px'
+          }} />
+        </Link>
       </main>
     </Layout>
   )
