e67eb2cc5ff5bef48bd5da3a27a1048e4f9a3ec1
Axel <axosar@gmail.com>
2021-09-17 20:12:11 -0500

    layout

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,21 @@
 import LayoutDashboard from './components/LayoutDashboard';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Dashboard(){
   return(
     <LayoutDashboard>
-      dashboard
+      <Container fluid={true}>
+        <Row>
+          <Col xs={2} md={2} lg={2}>
+            Menu
+          </Col>
+          <Col>
+          Contenido
+          </Col>
+        </Row>
+      </Container>
     </LayoutDashboard>
   )
 }
